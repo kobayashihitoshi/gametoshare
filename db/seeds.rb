96ab4f8e53804b1455d99d1c1752e7417010efeb
EC2 Default User <ec2-user@ip-172-31-30-5.ap-northeast-1.compute.internal>
2024-10-31 06:55:49 +0000

    3rdデプロイ+テスト完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create!(email: "admin@example.jp",
-  password:  "abcd1234",
-  password_confirmation: "abcd1234",
-  admin: true)
